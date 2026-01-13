98bfd63888fa8bd7bf62025f75511150d2918f1c
Forbidden_403 <terpesonaewidwud@gmail.com>
2026-01-13 18:49:46 +0700

    Delete danguard-v3.1_u directory

--- danguard-v3.1_u/claimers/base_claimer.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BaseClaimer(ABC):
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def claim(self, target_name: str, **kwargs) -> dict:
-        pass
